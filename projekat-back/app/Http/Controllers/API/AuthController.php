61dcde0b1798267735aef5c202dfe9c4d2c105ea
danilopaunovic1998 <57719918+danilopaunovic1998@users.noreply.github.com>
2022-09-03 21:34:22 +0200

    Sanctum integration (fun: register, login, logout)

---
+++ projekat-back/app/Http/Controllers/API/AuthController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'username' => 'required|string|max:255',
+            'email' => 'required|string|max:255|email|unique:users',
+            'password' => 'required|string|min:8',
+        ]);
+
+        if ($validator->fails())
+            return response()->json($validator->errors());
+
+        $user = User::create([
+            'username' => $request->username,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+        return response()->json(['data' => $user, 'access_token' => $token, 'token_type' => 'Bearer']);
+    }
+
+    public function login(Request $request)
+    {
+        if (!Auth::attempt($request->only('email', 'password')))
+            return response()->json(['message' => 'Unauthorized'], 401);
+
+        $user = User::where('email', $request['email'])->firstOrFail();
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json(['message' => 'Welcome ' . $user->username, 'acess_token' => $token, 'token_type' => 'Bearer']);
+    }
+
+    public function logout()
+    {
+        auth()->user()->tokens()->delete();
+        return [
+            'message' => 'You have logged out'
+        ];
+    }
+}
